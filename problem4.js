dbf876df2448587ec78f997f2b7a5fb7a1a9018c
Rifah01 <rifahshajida3@gmail.com>
2025-08-04 16:52:13 -0700

    problem4,5 are commited

---
+++ problem4.js
@@ -0,0 +1,16 @@
+/** Problem 04 - (Delete / Store) */
+
+var fileName="result.pdf" ;
+//write your code here
+
+if(fileName.includes("pdf")){
+    console.log("store")
+}else if(fileName.includes("docx")){
+    console.log("store")
+}else if(fileName.includes("#")){
+    console.log("store")
+}else{
+    console.log("Delete")
+}
+
+
---
+++ problem5.js
@@ -0,0 +1,8 @@
+
+var student={
+    name:"jhankar", 
+    roll: 1010 ,
+    department:"cse"
+}
+
+console.log(student.name + student.roll + "." + student.department +"@ph.ac.bd")
